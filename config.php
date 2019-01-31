c2439d7d607800256db0229cf6b625b1078b7451
jhonhansen <42970507+jhonhansen@users.noreply.github.com>
2019-01-31 23:27:33 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,13 +3,13 @@
 // user_name boleh kosong //
 $user_name="";
 //user_gaid wajib isi
-$user_gaid= "xxxxxxxxxxxxxxxxxx";
+$user_gaid= "c3e7832a-ae90-4601-bf81-221103c452f5";
 //user_token wajib di isi //
-$user_token="xxxxxxxxxxxxxxxxxx";
+$user_token="AbxYJHl4Qcboac2r";
 //user_id wajib di isi //
-$user_id="Facebook_xxxxxxxxxxxxxxxxxx";
+$user_id="Facebook_1047312888810011";
 // invite_code wajib di isi //
-$invite_code="xxxxxxxxxxxxxxxxxx";
+$invite_code="F4nOWu5olP";
 // Bearer wajib di isi //
-$Bearer="eyxxxxxxxxxxxxxxxxxx";
+$Bearer="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJGYWNlYm9va18xMDQ3MzEyODg4ODEwMDExIiwiZXhwIjoiMTU1MTU0MjM2NTAyOSJ9.b8s7klmbi8yVERDZvuWvT0BudunN020iA1UliJFFxtk";
 ?>
